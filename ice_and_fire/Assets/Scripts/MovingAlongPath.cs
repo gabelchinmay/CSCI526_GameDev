2e8baea1a44f703a5c8f3e41a7f0d9d84bec6d80
liuziyu-98 <zliu4183@usc.edu>
2023-10-07 00:01:46 -0700

    Chapter4_Level1 King's Landing Updates
    - Fixed the bug of moving platform: Now the player can move and jump on moving platforms as the normal platform.
    - Finished C4L1 King's Landing part for Ned
    - Created the platform roads for Robb and Arya

--- ice_and_fire/Assets/Scripts/MovingAlongPath.cs
+++ ice_and_fire/Assets/Scripts/MovingAlongPath.cs
@@ -10,7 +10,9 @@ public class MovingAlongPath : MonoBehaviour
     //Public Variables
     public Vector2[] setPoints;
     public float movingSpeed = 1.0f;
-    
+
+    private Vector2 playerOffset;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,6 @@ public class MovingAlongPath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, setPoints[currIndex], movingSpeed * Time.deltaTime);
         if (Vector2.Distance(transform.position, setPoints[currIndex]) < 0.02f)
         {
             currIndex++;
@@ -28,6 +29,30 @@ public class MovingAlongPath : MonoBehaviour
             {
                 currIndex = 0;
             }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, setPoints[currIndex], movingSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "player")
+        {
+           collision.gameObject.transform.SetParent(transform);
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // save player's position
+            Vector3 playerPosition = collision.transform.position;
+
+            collision.transform.SetParent(null);
+
+            // set player's position as the previous saved position
+            collision.transform.position = playerPosition;
         }
     }
 }
