763d2f0546c51b96dfb86915889b53ad6a8b5aee
Jack <jiancong@usc.edu>
2023-10-12 21:35:25 -0700

    function Enhance
    Attempt function Global Implemented

--- ice_and_fire/Assets/Scripts/GameOverScreen.cs
+++ ice_and_fire/Assets/Scripts/GameOverScreen.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
+    
+
     public void SetUp()
     {
         gameObject.SetActive(true);
@@ -12,25 +14,40 @@ public class GameOverScreen : MonoBehaviour
 
     public void Replay()
     {
+        SendToGoogle sendToGoogle = FindObjectOfType<SendToGoogle>();
+        sendToGoogle.PlayerAttempted();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        
     }
 
     public void Exit()
     {
+
+        SendToGoogle sendToGoogle = FindObjectOfType<SendToGoogle>();
+        sendToGoogle.PlayerPassedLevel();
         SceneManager.LoadScene(0);
+
     }
 
     public void MainMenu()
     {
+
+        SendToGoogle sendToGoogle = FindObjectOfType<SendToGoogle>();
+        sendToGoogle.PlayerPassedLevel();
         SceneManager.LoadScene(0);
     }
 
     public void ChooseLevel()
     {
+        SendToGoogle sendToGoogle = FindObjectOfType<SendToGoogle>();
+        sendToGoogle.PlayerPassedLevel();
+
         //SceneManager.LoadScene("ChooseLevel");
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("ChooseLevel");
         asyncLoad.completed += OnSceneLoaded;
 
+       
     }
 
     private void OnSceneLoaded(AsyncOperation asyncLoad)
--- ice_and_fire/Assets/Scripts/SendToGoogle.cs
+++ ice_and_fire/Assets/Scripts/SendToGoogle.cs
@@ -5,31 +5,27 @@ using UnityEngine.Networking;
 
 public class SendToGoogle : MonoBehaviour
 {
-    [SerializeField] private string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScqIicwiiIuDsO5TGK6yT5xSUBAgJ27RDnwecMRbEvIcEsNyQ/formResponse"; // 请将 Your_Form_ID 替换为实际的 Google 表单 ID
+    [SerializeField] private string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScqIicwiiIuDsO5TGK6yT5xSUBAgJ27RDnwecMRbEvIcEsNyQ/formResponse";
 
     public long sessionID;
     public int Chapter = 1;
     public int Level = 1;
-    public float Count = 0.0f; // time count
+    public float Count = 0.0f;
     public int TotalJump = 0;
+    public static int Attempts = 1; // 将Attempts变为静态变量
 
-    public static SendToGoogle currentSendToGoogle; // SendToGoogle 
+    public static SendToGoogle currentSendToGoogle;
 
     public void Start()
     {
-        // Assign sessionID to identify playtests
         sessionID = DateTime.Now.Ticks;
-
-        // set SendToGoogle
         currentSendToGoogle = this;
     }
 
     public void SetParameters(int chapter, int level)
     {
-
         this.Chapter = chapter;
         this.Level = level;
-
         if (this.Chapter == 1 && this.Level > 3)
         {
             this.Chapter++;
@@ -46,7 +42,10 @@ public class SendToGoogle : MonoBehaviour
             this.Level = 1;
         }
         Debug.Log("parameter passed");
+
         Debug.Log(chapter.ToString() + " " + level.ToString());
+
+        // 当关卡被设置时，重置尝试次数
     }
 
     public void addJump()
@@ -61,23 +60,38 @@ public class SendToGoogle : MonoBehaviour
 
     public void Send()
     {
-        // Assign variables
-        StartCoroutine(Post(sessionID.ToString(), Chapter.ToString(), Level.ToString(), Count.ToString(), TotalJump.ToString()));
+        StartCoroutine(Post(sessionID.ToString(), Attempts.ToString(), Chapter.ToString(), Level.ToString(), Count.ToString(), TotalJump.ToString()));
     }
 
-    public IEnumerator Post(string sessionID, string Chapter, string Level, string Time, string Jump)
+    public void PlayerAttempted()
+    {
+        Attempts++;
+        Debug.Log("Attempts: " + Attempts);
+    }
+
+    public void PlayerPassedLevel()
+    {
+        // 在玩家通过关卡时重置尝试次数
+        Attempts = 1;
+    }
+
+    public IEnumerator Post(string sessionID, string attempts, string Chapter, string Level, string Time, string Jump)
     {
         Debug.Log("begin to send");
-        // Create the form and enter responses
+        Debug.Log("Attempts: " + Attempts);
         WWWForm form = new WWWForm();
         form.AddField("entry.779211660", sessionID);
+        form.AddField("entry.1494050405", attempts);
         form.AddField("entry.1037631838", Chapter);
         form.AddField("entry.583117995", Level);
         form.AddField("entry.726422087", Time);
         form.AddField("entry.24347338", Jump);
 
+        // 添加玩家尝试次数到表单中
+        form.AddField("entry.123456789", Attempts.ToString());
+
         Debug.Log(form.ToString());
-        // Send responses and verify result
+
         using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
         {
             Debug.Log(www.result.ToString());
@@ -94,11 +108,10 @@ public class SendToGoogle : MonoBehaviour
                 Debug.Log("good");
                 Debug.Log("Form upload complete!");
 
-                // 重置时间和跳跃次数
                 Count = 0.0f;
                 TotalJump = 0;
+                Attempts = 1; // 在提交后重置尝试次数
 
-                // 根据关卡和章节条件增加 Chapter
                 www.Dispose();
             }
         }
