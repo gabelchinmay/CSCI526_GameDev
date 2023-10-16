78f1127ae1bb9938a02f683b99d95e418bf30b26
marcosu0412 <57587293+marcosu0412@users.noreply.github.com>
2023-10-16 15:17:44 -0700

    Add home button on choose level scene

---
+++ ice_and_fire/Assets/Scripts/BackHome.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackHome : MonoBehaviour
+{
+    public void Home()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
