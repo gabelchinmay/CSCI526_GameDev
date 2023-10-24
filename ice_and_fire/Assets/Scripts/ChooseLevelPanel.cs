378d5a3a3184baf85188142efd34ed5bae653c5e
marcosu0412 <57587293+marcosu0412@users.noreply.github.com>
2023-10-23 21:21:18 -0700

    Fixed chooseLevel error

---
+++ ice_and_fire/Assets/Scripts/ChooseLevelPanel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseLevelPanel : MonoBehaviour
+{
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+}
