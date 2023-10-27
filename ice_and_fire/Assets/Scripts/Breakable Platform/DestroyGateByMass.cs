3403f4484961c5813c89d85ca538999ca34ddac9
Ziyu Liu <zliu4183@usc.edu>
2023-10-26 18:40:19 -0700

    Compliation bug fix

--- ice_and_fire/Assets/Scripts/Breakable Platform/DestroyGateByMass.cs
+++ ice_and_fire/Assets/Scripts/Breakable Platform/DestroyGateByMass.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyGateByMass : MonoBehaviour
 {
     private float massThreshold = 5f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && (float) collision.gameObject.GetComponent<Rigidbody2D>().mass > massThreshold)
