197f09184f1c069e9e14eb295e0ca89f11494148
Ziyu Liu <zliu4183@usc.edu>
2023-10-26 23:57:29 -0700

    Update Breakable platform/gate and C2L1
    - Creating Melting effects

--- ice_and_fire/Assets/Scripts/Breakable Platform/DestroyPlatfromByMass.cs
+++ ice_and_fire/Assets/Scripts/Breakable Platform/DestroyPlatfromByMass.cs
@@ -7,25 +7,35 @@ public class DestroyPlatfromByMass : MonoBehaviour
 {
     private bool playerOnPlatform = false;
     private float massThreshold = 5f;
+    public float fadeSpeed = 1.0f;
+    private SpriteRenderer spriteRenderer;
+    private Color currentColor;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentColor = spriteRenderer.color;
+    }
+
+    private void Update()
+    {
+        if (playerOnPlatform)
+        { 
+            currentColor.a -= fadeSpeed * Time.deltaTime;
+            spriteRenderer.color = currentColor;
+
+            if (currentColor.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<Rigidbody2D>().mass >= massThreshold)
         {
             playerOnPlatform = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            if (playerOnPlatform && collision.gameObject.GetComponent<Rigidbody2D>().mass >= massThreshold)
-            {
-                Destroy(gameObject);
-            }
-
-            playerOnPlatform = false;
         }
     }
 }
